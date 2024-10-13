59871421fcba897c5c25b47be01970be956e6191
Jake130 <115515270+Jake130@users.noreply.github.com>
2024-10-13 16:36:53 -0700

    Added methods and typing to table.py

--- lstore/table.py
+++ lstore/table.py
@@ -15,11 +15,15 @@ class Record:
         self.columns = columns
 
 class Table:
-
     """
-    :param name: string         #Table name
-    :param num_columns: int     #Number of Columns: all columns are integer
-    :param key: int             #Index of table key in columns
+    Instantiates a table object
+
+    INPUTS:
+        -name:          string            #Table name
+        -num_columns:   int               #Number of Columns: all columns are integer
+        -key:           int               #Index of table key in columns
+    OUTPUT:
+        -table object
     """
     def __init__(self, name, num_columns, key):
         self.name = name
@@ -28,6 +32,28 @@ class Table:
         self.page_directory = {}
         self.index = Index(self)
         pass
+
+    def locate_record(self, RID:int):
+        """
+        Given the RID, locates the physical location of the
+        corresponding record.
+
+        INPUTS:
+            -RID:          int        #The record Id
+        OUTPUT:
+            The page the record is stored in, tupled with byte offset"""
+        pass
+
+    def add_page(self, col_number):
+        """
+        Adds a page when number of records exceeds page size
+
+        INPUTS
+            -col_number     int         The column that is being extended
+        """
+        pass
+
+
 
     def __merge(self):
         print("merge is happening")
