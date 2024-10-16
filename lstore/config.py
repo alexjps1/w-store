f099fe7a337e3333d1f86aa13a32d9f158982f46
Cory Johns <cory@triplevisiongroup.com>
2024-10-16 15:47:46 -0700

    feat: added comments and new constant to Config class

--- lstore/config.py
+++ lstore/config.py
@@ -9,4 +9,7 @@ class Config:
     Module Interfaces: None
     """
     def __init__(self) -> None:
+        # PAGE_SIZE is the byte capacity for each page
         self.PAGE_SIZE:int = 4096
+        # FIXED_PAGE_DATA_SIZE is the length in bytes of the data stored in each page (1 entry, for 1 column)
+        self.FIXED_PAGE_DATA_SIZE:int = 16 # TODO replace with actual value
