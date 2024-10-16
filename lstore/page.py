d4bfff9b6bcf12e789cc0e3f92026a4d3a095f54
Cory Johns <cory@triplevisiongroup.com>
2024-10-16 15:51:40 -0700

    feat: finished first draft of Page class:
    - write function assumes has_capacity has already passed and input data
      is a bytearray

--- lstore/page.py
+++ lstore/page.py
@@ -1,16 +1,14 @@
 """
 Module Interfaces:
     Config for page size
-    Record for writing records to the page
 """
 from lstore.config import Config
 config = Config()
 
-from lstore.table import Record
 
 class Page:
     """
-    Page represents one column in the database. Records in the page are stored in a bytearray of length set by the PAGE_SIZE constant in the Config class.
+    Page represents one column in the database. one-column-partal-records in the page are stored in a bytearray of length set by the PAGE_SIZE constant in the Config class.
     The Page class can:
         - Check if a new record can be added
         - Write a new record
@@ -22,19 +20,22 @@ class Page:
 
     def has_capacity(self) -> bool:
         """
-        Function checks if this page has enough space left for a new record to be added.
+        Function checks if this page has enough space left for a new record (1 column of a record) to be added.
 
-        Inputs: None for fixed size records || new_record_size:int for variable sized records
+        Inputs: None for fixed size records
         Outputs: True if the page has enough space for the new record, otherwise False
         """
-        pass
+        return config.FIXED_PAGE_DATA_SIZE * self.num_records <= config.PAGE_SIZE
 
-    def write(self, value:Record) -> None:
+    def write(self, value:bytearray) -> None:
         """
-        Writes a new record to the page, and increments num_records by one.
-        Inputs: value the new record that will be written to the page
+        Writes a new bytearray to the page, and increments num_records by one.
+        Inputs: value that will be written to the page
         Outputs: None
         """
+        # we have +1 records in this column
         self.num_records += 1
-        pass
-
+        # this is the location of the start of this page entry
+        offset = config.FIXED_PAGE_DATA_SIZE * self.num_records
+        # set the data at the calculated offset
+        self.data[offset:offset + config.FIXED_PAGE_DATA_SIZE] = value
