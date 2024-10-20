2d3528b95a7d41e936efb4e1537ccbb01360c85f
Eric Edwards <eric.edwards02@gmail.com>
2024-10-20 13:48:55 -0700

    feat: defined RID bit size configuration

--- lstore/config.py
+++ lstore/config.py
@@ -12,4 +12,14 @@ class Config:
         # PAGE_SIZE is the byte capacity for each page
         self.PAGE_SIZE:int = 4096
         # FIXED_PAGE_DATA_SIZE is the length in bytes of the data stored in each page (1 entry, for 1 column)
-        self.FIXED_PAGE_DATA_SIZE:int = 16 # TODO replace with actual value
+        self.FIXED_PAGE_DATA_SIZE:int = 32 # TODO replace with actual value
+
+        # define RID attribute bit sizes
+        # This defines the following constraints under the (1, 21, 10) = 32 format
+        # - Only 4 billion inserts possible
+        # - Max of 32 columns
+        #  - Only supports 4 byte integer values
+        self.TAIL_BIT = 1
+        self.PAGE_NUMBER_BITS = 21
+        self.OFFSET_BITS = 10
+        self.RID_BIT_SIZE = self.TAIL_BIT + self.PAGE_NUMBER_BITS + self.OFFSET_BITS
