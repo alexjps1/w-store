e78ca5346b76dec492e708237f9d55e2a8ed33fd
Cory Johns <cory@triplevisiongroup.com>
2024-10-23 15:00:20 -0700

    feat: first draft of query update if done:
    - page directory has changed
    - assignment given update function does not support non-cumulative
      tail records
    - implementation also assumes insert will set a new base record's RID to
      both it's RID_COLUMN and its INDIRECTION_COLUMN

--- lstore/query.py
+++ lstore/query.py
@@ -1,6 +1,7 @@
 from lstore.table import Table, Record
 from lstore.index import Index
 from typing import Literal, Any
+from lstore.config import NUM_METADATA_COLUMNS, INDIRECTION_COLUMN, RID_COLUMN, SCHEMA_ENCODING_COLUMN
 
 
 class Query:
@@ -18,7 +19,7 @@ class Query:
     """
     # internal Method
     # Read a record with specified RID
-    # Returns True upon succesful deletion
+    # Returns True upon successful deletion
     # Return False if record doesn't exist or is locked due to 2PL
     """
     def delete(self, primary_key):
@@ -27,7 +28,7 @@ class Query:
     
     """
     # Insert a record with specified columns
-    # Return True upon succesful insertion
+    # Return True upon successful insertion
     # Returns False if insert fails for whatever reason
     """
     def insert(self, *columns):
@@ -47,7 +48,7 @@ class Query:
     def select(self, search_key:Any, search_key_index:int, projected_columns_index:list[bool]) -> list[Record]|Literal[False]:
         # find the Record IDs
         rids = self.table.index.locate(search_key_index, search_key)
-        # get relevent columns for the records
+        # get relevant columns for the records
         records = [self.table.locate_record(rid, search_key, projected_columns_index, 0) for rid in rids]
         return records
 
@@ -57,7 +58,7 @@ class Query:
     # :param search_key: the value you want to search based on
     # :param search_key_index: the column index you want to search based on
     # :param projected_columns_index: what columns to return. array of 1 or 0 values.
-    # :param relative_version: the relative version of the record you need to retreive.
+    # :param relative_version: the relative version of the record you need to retrieve.
     # Returns a list of Record objects upon success
     # Returns False if record locked by TPL
     # Assume that select will never be called on a key that doesn't exist
@@ -65,18 +66,22 @@ class Query:
     def select_version(self, search_key, search_key_index, projected_columns_index, relative_version):
         # find the Record IDs
         rids = self.table.index.locate(search_key_index, search_key)
-        # get relevent columns for the records
+        # get relevant columns for the records
         records = [self.table.locate_record(rid, search_key, projected_columns_index, relative_version) for rid in rids]
         return records
 
     
     """
     # Update a record with specified key and columns
-    # Returns True if update is succesful
+    # Returns True if update is successful
     # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking
     """
     def update(self, primary_key, *columns):
-        pass
+        # find the base record with primary_key
+        # TODO return False if index fails to find rid
+        rid = self.table.index.locate(NUM_METADATA_COLUMNS, primary_key)[0]
+        # append new tail record with *columns, and indirection to other tail record's RID and set base record's indirection to new tail's RID
+        return self.table.append_tail_record(rid, columns)
 
     
     """
@@ -95,7 +100,7 @@ class Query:
     :param start_range: int         # Start of the key range to aggregate 
     :param end_range: int           # End of the key range to aggregate 
     :param aggregate_columns: int  # Index of desired column to aggregate
-    :param relative_version: the relative version of the record you need to retreive.
+    :param relative_version: the relative version of the record you need to retrieve.
     # this function is only called on the primary key.
     # Returns the summation of the given range upon success
     # Returns False if no record exists in the given range
@@ -105,7 +110,7 @@ class Query:
 
     
     """
-    incremenets one column of the record
+    increments one column of the record
     this implementation should work if your select and update queries already work
     :param key: the primary of key of the record to increment
     :param column: the column to increment
--- lstore/table.py
+++ lstore/table.py
@@ -1,10 +1,10 @@
 from lstore.index import Index
 from time import time
 from lstore.page import Page
-from typing import List
+from typing import List, Literal
 from lstore.config import RID_COLUMN, INDIRECTION_COLUMN, SCHEMA_ENCODING_COLUMN, TIMESTAMP_COLUMN, MAX_COLUMNS, NUM_METADATA_COLUMNS
 from lstore.config import schema_AND, schema_SUBTRACT, bytearray_to_int, bytearray_to_schema, int_to_bytearray, schema_to_bytearray
-from rid import rid_to_coords
+from rid import rid_to_coords, coords_to_rid
 
 
 class Record:
@@ -38,14 +38,14 @@ class Table:
         assert self.num_columns <= MAX_COLUMNS
         # add metadata columns
         self.page_directory = { 
-                                RID_COLUMN : [ Page() ],
-                                INDIRECTION_COLUMN : [ Page() ],                #all pages associated with base column
-                                SCHEMA_ENCODING_COLUMN : [ Page() ],
-                                TIMESTAMP_COLUMN : [ Page() ],
+                               RID_COLUMN : {"base":[ Page() ], "tail":[]},
+                                INDIRECTION_COLUMN : {"base":[ Page() ], "tail":[]},                #all pages associated with base column
+                                SCHEMA_ENCODING_COLUMN : {"base":[ Page() ], "tail":[]},
+                                TIMESTAMP_COLUMN : {"base":[ Page() ], "tail":[]},
                                 }
         # add data columns
         for i in range(num_columns):
-            self.page_directory[NUM_METADATA_COLUMNS + i] = [ Page() ]
+            self.page_directory[NUM_METADATA_COLUMNS + i] = {"base":[ Page() ], "tail":[]}
 
         self.index = Index(self)
         pass
@@ -53,7 +53,78 @@ class Table:
     def insert_record_into_pages(self, schema, *columns):
         pass
 
-    def locate_record(self, RID: int, key:int, column_mask:list[bool], version:int=0) -> Record:
+    def append_tail_record(self, base_RID:int, columns:list[int]) -> bool:
+        """
+        Appends a new tail record for the given column updates. This assumes cumulative tail records. This is because the tail record RIDs require that the page entries for tail records line up. So even the non-cumulative implementation will require null values to be written to non-updated columns.
+        Also assumes new base records default to their RID in both the RID_COLUMN and INDIRECTION_COLUMN.
+        """
+        # append new tail record with *columns, and indirection to other tail record's RID
+        # find the most recent tail record from base record's indirection
+        # TODO check tail != base, or that Base Records default to their RIDs in the INDIRECTION_COLUMN instead of a null value
+        old_tail_rid = self.get_partial_record(base_RID, INDIRECTION_COLUMN)
+        schema_encoding = bytearray([1]*len(columns)) # NOTE this is wrong for incomplete updates
+        timestamp = int(time()) # accurate to the second only
+        # get the page to append the new tail record rid
+        page = self.get_writable_page(RID_COLUMN)
+        # get info for new rid
+        offset = page.num_records
+        page_num = self.page_directory[RID_COLUMN]["tail"].Index(page)
+        # create the new rid
+        new_tail_rid = coords_to_rid(True, page_num, offset)
+
+        # write the metadata columns
+        write_cols:list[int] = [INDIRECTION_COLUMN, SCHEMA_ENCODING_COLUMN, TIMESTAMP_COLUMN]
+        write_vals:list[bytearray] = [old_tail_rid, schema_encoding, int_to_bytearray(timestamp)]
+        # write Tail RID
+        page.write_direct(int_to_bytearray(new_tail_rid)) 
+        # the rid page number is needed for RID generation, so it is redundant to include writing the rid in the for loop
+        for i, col in enumerate(write_cols):
+            page = self.get_writable_page(col)
+            page.write_direct(write_vals[i])
+        
+        # write data columns
+        for i, col in enumerate(columns):
+            # write data to page
+            page = self.get_writable_page(i + NUM_METADATA_COLUMNS)
+            page.write_direct(int_to_bytearray(col))
+
+        # set base record's indirection to new tail's RID
+        _, page_num, offset = rid_to_coords(base_RID)
+        base_page = self.page_directory[INDIRECTION_COLUMN]["base"][page_num]
+        base_page.overwrite_direct(int_to_bytearray(new_tail_rid), offset)
+
+        # update was successful
+        return True
+
+    def get_writable_page(self, column:int, key:Literal["tail"]|Literal["base"]="tail") -> Page:
+        """
+        Obtains a tail/base page for the specified column with space for at least one write. NOTE if a new page needs to be allocated, a new page will be added for all columns, since the RIDs require pages to be lined up across columns.
+
+        Input: column, the column the page is part of
+        Output: the page object
+        """
+        add_new = False
+        if len(self.page_directory[column][key]) != 0:
+            page = self.page_directory[column][key][-1]
+            if not page.has_capacity():
+                # add a new page
+                add_new = True
+            else:
+                return page
+        else:
+            # add the first new page
+            add_new = True
+
+        if add_new:
+            # add new page for all columns
+            for col in self.page_directory.keys():
+                self.add_page(col, is_tail_page=(key=="tail"))
+        
+        page = self.page_directory[column][key][-1]
+        # return the Page 
+        return page
+
+    def locate_record(self, RID: int, key:int, column_mask:list[bool]|list[int]|bytearray, version:int=0) -> Record:
         """
         Given the RID, provides the record with that RID via indexing.
 
@@ -83,13 +154,13 @@ class Table:
         # apply the tail records to base record and return columns as directed
         return self.apply_tails_to_base(tail_RID, RID, key, column_mask)
 
-    def apply_tails_to_base(self, Tail_RID:int, Base_RID:int, key:int, column_mask:list[bool]) -> Record:
+    def apply_tails_to_base(self, Tail_RID:int, Base_RID:int, key:int, column_mask:list[bool]|bytearray) -> Record:
         """
-        Calculates the record represented by the given tail record.
+        Calculates the record represented by the given tail record. Using non-cumulative tail records.
         """
         # go through tail records until all columns in column_mask have been found in schema, or base record is hit
         # columns may not be added in order, and some may not be in the tail records
-        columns = [None]*len(column_mask)
+        columns = bytearray(len(column_mask))
         working_mask = column_mask
         tail_rid = Tail_RID
         while True in working_mask:
@@ -117,7 +188,7 @@ class Table:
         columns = [col for col in columns if col is not None]
         return Record(Base_RID, key, columns)
 
-    def locate_base_record(self, RID:int, key:int, column_mask:list[bool]) -> Record:
+    def locate_base_record(self, RID:int, key:int, column_mask:list[bool]|bytearray) -> Record:
         # for given column_mask:
         columns = []
         for i, mask in enumerate(column_mask):
@@ -126,7 +197,7 @@ class Table:
         # build the record object
         return Record(RID, key, columns)
 
-    def get_partial_record(self, RID:int, column:int) -> int|list[bool]:
+    def get_partial_record(self, RID:int|bytearray, column:int) -> bytearray:
         """
         Accepts the RID and the column number and returns the partial record contained at the correct page and offset.
         
@@ -134,34 +205,33 @@ class Table:
             RID, the record id
             column: the column index of interest
         Outputs:
-            the partial record data as an int or list of 0 and 1 for schema
+            the partial record data: bytearray representing an int or list of 0 and 1 for schema
         """
-        # get RID and offset
-        tail, page_num, offset = rid_to_coords(RID)
-        # get raw data
-        data = self.page_directory[column][page_num].retrieve_direct(offset)
-        # convert from bytearray
-        if column == SCHEMA_ENCODING_COLUMN:
-            # process data as schema
-            return bytearray_to_schema(data)
-        # elif column == TIMESTAMP_COLUMN:
-        #     # process data as time
-        #     pass
+        if type(RID) == bytearray:
+            rid = bytearray_to_int(RID)
         else:
-            # process data as int
-            # use for data columns, RIDs, and Indirection, TODO also timestamp?
-            return bytearray_to_int(data)
+            rid = RID
+        # get RID and offset
+        tail, page_num, offset = rid_to_coords(rid)
+        # get raw data
+        if tail:
+            data = self.page_directory[column]["tail"][page_num].retrieve_direct(offset)
+        else:
+            data = self.page_directory[column]["base"][page_num].retrieve_direct(offset)
+        return data
 
-
-    def add_page(self, col_number):
+    def add_page(self, col_number:int, is_tail_page:bool=False):
         """
         Adds a page when number of records exceeds page size
 
         INPUTS
             -col_number     int         The column that is being extended
         """
-        self.page_directory[col_number].append(Page())
-        pass
+        if is_tail_page:
+            page_type = "tail"
+        else:
+            page_type = "base"
+        self.page_directory[col_number][page_type].append(Page())
 
     def __merge(self):
         print("merge is happening")
