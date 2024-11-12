05c5b68993d232b2d872cf79e7333841b69b6b07
Cory Johns <cory@triplevisiongroup.com>
2024-11-12 10:50:13 -0800

    chore: updated placeholder_index to new page directory:
    - it is now even slower

--- lstore/placeholder_index.py
+++ lstore/placeholder_index.py
@@ -1,7 +1,5 @@
 from lstore.config import RID_COLUMN, NUM_METADATA_COLUMNS
 from lstore.config import bytearray_to_int
-# from lstore.table import Table
-from typing import Literal
 
 class DumbIndex:
     """
@@ -11,17 +9,23 @@ class DumbIndex:
         self.table = table
 
     def get_rid(self, page_num:int, offset:int) -> int:
-        return bytearray_to_int(self.table.page_directory[RID_COLUMN]["base"][page_num].retrieve_direct(offset))
+        page = self.table.page_directory.retrieve_page(RID_COLUMN, False, page_num)
+        if page is not None:
+            return bytearray_to_int(page.retrieve_direct(offset))
+        else:
+            assert False
 
     def locate(self, column_num:int, value:int) -> list[int]:
         rids:list[int] = []
         # check all pages
-        for n, page in enumerate(self.table.page_directory[column_num + NUM_METADATA_COLUMNS]["base"]):
-            # check all offsets
-            for i in range(page.num_records):
-                if bytearray_to_int(page.retrieve_direct(i)) == value:
-                    # add rid to locate list
-                    rids.append(self.get_rid(n, i))
+        for n in range(self.table.num_columns):
+            page = self.table.page_directory.retrieve_page(column_num + NUM_METADATA_COLUMNS, False, n)
+            if page is not None:
+                # check all offsets
+                for i in range(page.num_records):
+                    if bytearray_to_int(page.retrieve_direct(i)) == value:
+                        # add rid to locate list
+                        rids.append(self.get_rid(n, i))
         return rids
             
 
@@ -37,13 +41,15 @@ class DumbIndex:
         """
         rids:list[int] = []
         # check all pages for start_val, values may not be sorted
-        for n, page in enumerate(self.table.page_directory[0 + NUM_METADATA_COLUMNS]["base"]):
-            # check all offsets
-            for i in range(page.num_records):
-                # if the value is in range add it
-                if bytearray_to_int(page.retrieve_direct(i)) >= start_key and bytearray_to_int(page.retrieve_direct(i)) <= end_key:
-                    # assuming inclusive range
-                    rids.append(self.get_rid(n, i))
+        for n in range(self.table.num_columns):
+            page = self.table.page_directory.retrieve_page(0 + NUM_METADATA_COLUMNS, False, n)
+            if page is not None:
+                # check all offsets
+                for i in range(page.num_records):
+                    # if the value is in range add it
+                    if bytearray_to_int(page.retrieve_direct(i)) >= start_key and bytearray_to_int(page.retrieve_direct(i)) <= end_key:
+                        # assuming inclusive range
+                        rids.append(self.get_rid(n, i))
         # return values only if both start and end were found
         return sorted(rids)
 
