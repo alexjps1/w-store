b9ed787de867756277116a2ed57b11f6dbe264da
Cory Johns <cory@triplevisiongroup.com>
2024-11-24 15:29:03 -0800

    feat: added locking to query:
    - select will access the base and tail records in a read-only fashion,
      but shared locks are currently only assigned to the base record.
    - code is not tested

--- lstore/query.py
+++ lstore/query.py
@@ -1,4 +1,5 @@
 from lstore.table import Table, Record
+from lstore.lock_manager import LockManager, INDEX, PAGE_DIR
 # from lstore.index import Index
 from typing import Literal
 
@@ -12,7 +13,7 @@ class Query:
     """
     def __init__(self, table):
         self.table = table
-        pass
+        self.lock_manager = LockManager()
 
 
     """
@@ -24,12 +25,16 @@ class Query:
     def delete(self, primary_key:int) -> bool:
         # find the record
         rid = self.table.index.locate(0, primary_key)[0]
-        # try:
-        #     rid = self.table.index.locate(0, primary_key)[0]
-        # except IndexError:
-        #     return False
-        # delete record and return success state
-        return self.table.delete_record(rid)
+        result = self.lock_manager.get_record_lock(rid, True)
+        if result:
+            # delete record and return success state
+            value = self.table.delete_record(rid)
+            # release lock
+            self.lock_manager.release_record_lock(rid, True)
+            return value
+        else:
+            # failed to acquire lock
+            return False
 
 
     """
@@ -78,8 +83,19 @@ class Query:
         rids = self.table.index.locate_version(search_key_index, search_key, relative_version)
         if rids is False or len(rids) == 0:
             return False
+        locks = [self.lock_manager.get_record_lock(rid, False) for rid in rids]
+        if locks.__contains__(False):
+            # didn't get a lock
+            for i, rid in enumerate(rids):
+                # remove all locks that did acquire
+                if locks[i]:
+                    self.lock_manager.release_record_lock(rid, False)
+            return False
         # get relevant columns for the records
         records = [self.table.locate_record(rid, search_key, projected_columns_index, relative_version) for rid in rids]
+        # release all locks
+        for rid in rids:
+            self.lock_manager.release_record_lock(rid, False)
         return records
 
 
@@ -104,8 +120,16 @@ class Query:
                 # update failed because primary key already exists
                 # print(f"Skipping DUPLICATE Tail for Base RID::{rid}")
                 return False
-        # primary key is not being updated, or it is and wasn't already in the table
-        return self.table.append_tail_record(rid, columns)
+        # acquire locks
+        lock = self.lock_manager.get_record_lock(rid, True)
+        if lock:
+            # primary key is not being updated, or it is and wasn't already in the table
+            result = self.table.append_tail_record(rid, columns)
+            # release lock
+            self.lock_manager.release_record_lock(rid, True)
+            return result
+        else:
+            return False
 
 
     """
@@ -131,10 +155,20 @@ class Query:
     """
     def sum_version(self, start_range:int, end_range:int, aggregate_column_index:int, relative_version:int) -> int|Literal[False]:
         # ask index to find the relevant RIDs
-        # TODO return False if no records where found
         # print("searching for rids", start_range, end_range, aggregate_column_index)
         # using col_num 0 becasue that is the primary key's index
         rid_set = self.table.index.locate_range(start_range, end_range, 0)
+        if len(rid_set) == 0:
+            return False
+        # get locks for all rids in sum
+        locks = [self.lock_manager.get_record_lock(rid, False) for rid in rid_set]
+        if locks.__contains__(False):
+            # didn't get a lock
+            for i, rid in enumerate(rid_set):
+                # remove all locks that did acquire
+                if locks[i]:
+                    self.lock_manager.release_record_lock(rid, False)
+            return False
         # print("rid set", rid_set)
         # get the attribute values and return the sum
         # build a all 0 column mask except for the aggregate column
@@ -147,6 +181,9 @@ class Query:
             record = self.table.locate_record(rid, 0, column_mask, relative_version)
             sum_value += record.columns[aggregate_column_index]
             # print("sum value", sum_value)
+        # release all locks
+        for rid in rid_set:
+            self.lock_manager.release_record_lock(rid, False)
         return sum_value
 
 
