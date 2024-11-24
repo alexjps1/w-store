2a335a896c40676109a44990533a12fe68e24589
Cory Johns <cory@triplevisiongroup.com>
2024-11-24 12:14:45 -0800

    feat: added transaction test to wstore_tester:
    - only tests adding/updating records with the Transaction class

--- wstore_tester.py
+++ wstore_tester.py
@@ -1,9 +1,11 @@
 from lstore.db import Database
 from lstore.query import Query
+from lstore.transaction import Transaction
+from random import shuffle
 
 db_name = "Project2Test"
 num_col = 5
-num_records = 800
+num_records = 8
 updates_per_record = 6
 
 
@@ -57,6 +59,9 @@ def build_records(update_column_selector=lambda x: x%(num_col - 1) + 1) -> tuple
             l[m] = i + n + m + 8
             updates.append(l)
         tail_records.append(updates)
+    # shuffle(primary_keys)
+    # shuffle(base_records)
+    # shuffle(tail_records)
     return primary_keys, base_records, tail_records
 
 def insert_into_table(query:Query, primary_keys:list[int], base_records:list[list[int]], tail_records:list[list[list[int|None]]]) -> None:
@@ -75,6 +80,26 @@ def insert_into_table(query:Query, primary_keys:list[int], base_records:list[lis
             query.update(key, *u)
             n += 1
     print(f"Appended {n} Tail Records")
+
+def transaction_into_table(query:Query, table:"Table", primary_keys:list[int], base_records:list[list[int]], tail_records:list[list[list[int|None]]]) -> None:
+    # print(f"BASE RECORDS::\n{base_records}\nTAIL RECORDS::\n{tail_records}")
+    # perform queries
+    transaction = Transaction()
+    print("building transaction")
+    n = 0
+    for r in base_records:
+        transaction.add_query(query.insert, table, *r)
+        # query.insert(*r)
+        n += 1
+    n = 0
+    for i, key in enumerate(primary_keys):
+        for u in tail_records[i]:
+            transaction.add_query(query.update, table, key, *u)
+            # query.update(key, *u)
+            n += 1
+    print("running transaction")
+    r = transaction.run()
+    print("transaction successful" if r else "transaction failed")
 
 def version_tester(name:str):
     """
@@ -136,6 +161,36 @@ def uneven_updates_tester(name:str):
     print("closing database")
     db.close()
 
+def test_transaction(name:str):
+    """
+    """
+    # Test new database
+    db = Database()
+    db.open(db_name)
+    table = db.create_table(name, num_col, 0)
+    query = Query(table)
+
+    primary_keys, base_records, tail_records = build_records(update_column_selector=lambda x:1)
+    transaction_into_table(query, table, primary_keys, base_records, tail_records)
+
+    print("\n-- Testing transaction on NEW Table --\n")
+    # compare solutions to query results
+    test_select_version(primary_keys, base_records, tail_records, query)
+    # print("closing database")
+    db.close()
+
+    # test loading a existing table
+    print("\n-- Testing transaction on LOADED Table --\n")
+    db = Database()
+    db.open(db_name)
+    table = db.get_table(name)
+    query = Query(table)
+    # compare solutions to query results
+    test_select_version(primary_keys, base_records, tail_records, query)
+    # print("closing database")
+    db.close()
+
 if __name__ == "__main__":
     version_tester("VersionTable")
     uneven_updates_tester("UnevenTable")
+    test_transaction("TransactionTable")
