c591d90178d95cfab88794798597a7dbd7becf2e
Alex JPS <git@alexjps.com>
2024-12-03 19:25:54 -0800

    chore: Calls to load/save index in db.py

--- lstore/db.py
+++ lstore/db.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 from lstore.table import Table
-from lstore.config import DATABASE_DIR, NUM_METADATA_COLUMNS, FIXED_PARTIAL_RECORD_SIZE, RID_COLUMN, OVERRIDE_WITH_DUMB_INDEX
+from lstore.config import DATABASE_DIR, NUM_METADATA_COLUMNS, FIXED_PARTIAL_RECORD_SIZE, RID_COLUMN, \
+    OVERRIDE_WITH_DUMB_INDEX
 from lstore.config import bytearray_to_int
 from lstore.config import debug_print as print
 from typing import Literal
+
 
 class Database():
     """
@@ -13,13 +15,14 @@ class Database():
         - Loading the database
         - Creation and deletion of tables (create and drop functions)
     """
+
     def __init__(self) -> None:
         # set of tables, identified by their name
-        self.tables:dict[str, Table] = {}   # this assumes no 2 tables have the same name
-        self.database_path:Path = Path("default")
+        self.tables: dict[str, Table] = {}  # this assumes no 2 tables have the same name
+        self.database_path: Path = Path("default")
         self.table_info_file = "__table_info__.bin"
 
-    def open(self, path:str):
+    def open(self, path: str):
         """
         Loads the database from disk.
         Inputs: path, the location of the database.
@@ -34,6 +37,7 @@ class Database():
             for file in full_path.iterdir():
                 if file.is_dir():
                     # print(f"loading table {file.stem}")
+                    # load the table (along with its index) from disk
                     self.get_table(str(file.stem))
                 # print(f"finished loading table {file.stem}")
             # print(f"finished loading database {path}")
@@ -48,6 +52,7 @@ class Database():
         # save database to disk
         for table in self.tables.values():
             table.page_directory.save_all()
+            table.index.save_index_to_disk(Path(DATABASE_DIR, table.name))
 
     """
     # Creates a new table
@@ -55,7 +60,8 @@ class Database():
     :param num_columns: int     #Number of Columns: all columns are integer
     :param key: int             #Index of table key in columns
     """
-    def create_table(self, name:str, num_columns:int, key_index:int, **kwargs) -> Table:
+
+    def create_table(self, name: str, num_columns: int, key_index: int, **kwargs) -> Table:
         """
         Creates a new table and returns it.
 
@@ -84,11 +90,11 @@ class Database():
         self.tables[name] = table
         return table
 
-    
     """
     # Deletes the specified table
     """
-    def drop_table(self, name:str) -> None:
+
+    def drop_table(self, name: str) -> None:
         """
         Deletes the table with the given name.
 
@@ -105,17 +111,17 @@ class Database():
             dir_name = Path(DATABASE_DIR, self.database_path, name)
             if dir_name.exists():
                 for file in dir_name.iterdir():
-                    #Remove file if it is a file
+                    # Remove file if it is a file
                     if file.is_file():
                         file.unlink()
-                #Delete the table
+                # Delete the table
                 dir_name.rmdir()
 
-    
     """
     # Returns table with the passed name
     """
-    def get_table(self, name:str) -> Table|Literal[False]:
+
+    def get_table(self, name: str) -> Table | Literal[False]:
         """
         Finds and returns the table with the given name.
 
@@ -136,71 +142,12 @@ class Database():
             # build table object
             table = Table(name, self.database_path, num_col, key_index)
             # regenerate the index
-            if not OVERRIDE_WITH_DUMB_INDEX: self.generate_index_on_loaded_table(path, table)
+            # if not OVERRIDE_WITH_DUMB_INDEX: self.generate_index_on_loaded_table(path, table)
+            # load the index from disk
+            table.index.load_index_from_disk(path)
             # update table dictionary
             self.tables[name] = table
             # return the table
             return table
         else:
             return False
-
-    def generate_index_on_loaded_table(self, path:Path, table:Table) -> None:
-        """
-        Generates a new index by reading saved page files. Used to generate a new index when a table is loaded, but not when a new table is made.
-        """
-        all_base_pages = sorted(path.glob("b_col*_*.bin"))
-        last_values = {}
-        # generate an index on all base records in the data columns
-        for page in all_base_pages:
-            # generate an index on all data columns
-            _, column_str, page_num = str(page.stem).split('_')
-            column_num = int(column_str[3:])
-            # ignore all metadata columns
-            if column_num >= NUM_METADATA_COLUMNS:
-                # is a data column, generate an index
-                # read data page
-                page_data = bytearray(page.read_bytes())
-                num_records = bytearray_to_int(page_data[:FIXED_PARTIAL_RECORD_SIZE])
-                data = page_data[FIXED_PARTIAL_RECORD_SIZE:]
-                # read rid page
-                rid_page = Path(path, f"b_col{RID_COLUMN}_{page_num}.bin")
-                rid_page_data = bytearray(rid_page.read_bytes())
-                rid_data = rid_page_data[FIXED_PARTIAL_RECORD_SIZE:]
-                # build an index entry for all records in page
-                for offset in range(num_records):
-                    byte_offset = offset * FIXED_PARTIAL_RECORD_SIZE
-                    # get rid
-                    rid = bytearray_to_int(rid_data[byte_offset:byte_offset + FIXED_PARTIAL_RECORD_SIZE])
-                    # get column value
-                    value = bytearray_to_int(data[byte_offset:byte_offset + FIXED_PARTIAL_RECORD_SIZE])
-                    # save for tail record indexes
-                    last_values[rid] = value
-                    # add index entry
-                    table.index.add_record_to_index(column_num, value, rid)
-
-        all_tail_pages = sorted(path.glob("t_col*_*.bin"))
-        # generate an index on all tail records in the data columns
-        for page in all_tail_pages:
-            # generate an index on all data columns
-            _, column_str, page_num = str(page.stem).split('_')
-            column_num = int(column_str[3:])
-            # ignore all metadata columns
-            if column_num >= NUM_METADATA_COLUMNS:
-                # is a data column, generate an index
-                # read data page
-                page_data = bytearray(page.read_bytes())
-                num_records = bytearray_to_int(page_data[:FIXED_PARTIAL_RECORD_SIZE])
-                data = page_data[FIXED_PARTIAL_RECORD_SIZE:]
-                # read rid page
-                base_rid_page = Path(path, f"b_col{RID_COLUMN}_{page_num}.bin")
-                base_rid_page_data = bytearray(base_rid_page.read_bytes())
-                base_rid_data = base_rid_page_data[FIXED_PARTIAL_RECORD_SIZE:]
-                # build an index entry for all records in page
-                for offset in range(num_records):
-                    byte_offset = offset * FIXED_PARTIAL_RECORD_SIZE
-                    # get rid
-                    base_rid = bytearray_to_int(base_rid_data[byte_offset:byte_offset + FIXED_PARTIAL_RECORD_SIZE])
-                    # get column value
-                    value = bytearray_to_int(data[byte_offset:byte_offset + FIXED_PARTIAL_RECORD_SIZE])
-                    # add index entry
-                    table.index.update_record_in_index(column_num, last_values[base_rid], base_rid, value)
